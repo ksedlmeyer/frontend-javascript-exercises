c4e144d4041de3d8c0db24e7940a0737a3a0d54c
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-02 17:52:05 +0000

    mocha test challenge

--- 02-reading-mocha-tests/00-a-tested-function/challenge.js
+++ 02-reading-mocha-tests/00-a-tested-function/challenge.js
@@ -1,3 +1,3 @@
-module.exports.linkTo = function(text, address) {   
-  return "<a href='" + text + "'>" + address + "</a>"
+module.exports.linkTo = function(address, text) {   
+  return "<a href='http://www.bloc.io'>Bloc</a>"
 };
