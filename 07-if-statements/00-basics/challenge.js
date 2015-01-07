7d69a02e71f291b28fd215a9953a7f331e8aa962
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-07 17:01:00 +0000

    If/else challenge completed

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,38 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(x, y) {
+  if (x === y) {
+    return "You got it!";
+  }
+  else if (x < y) {
+    return "Too high";
+  }
+  else {
+    return "Too low";
+  }
+}
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(w, x, y, z) {
+  if ((w===3 || w===5 || w===7) && (x===2) && (5<= y && y<=100) && (z<9 || z>20)) {
+    return "correct";
+  }
+  else {
+    return "incorrect";
+  }
+}
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  if (item === 'MacBook Air' && money >= 999) {
+    return true;
+  }
+  else if (item === 'MacBook Pro' && money >= 1299) {
+    return true;
+  }
+  else if (item === 'Mac Pro' && money >= 2499) {
+    return true;
+  }
+  else if (item === 'Apple Sticker' && money >= 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
