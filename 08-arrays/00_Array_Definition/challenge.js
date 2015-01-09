0af0cee7d21582582862922114aef5a261a4abc2
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-09 21:48:33 +0000

    Array exercise one

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,7 @@
-module.exports.newArray = undefined;
-
-module.exports.firstAndLast = undefined;
+module.exports.newArray = function(w, x, y, z) {
+  return [w, x, y, z]
+}
+module.exports.firstAndLast = function(array) {
+  if (array.length < 3) return;
+  return [array.shift(), array.pop()];  
+}
