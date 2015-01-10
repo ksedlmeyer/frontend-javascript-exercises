89662411ab557823eee22816e5c96288b3f055a5
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-10 00:12:23 +0000

    Array functions challenge

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,13 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array) {
+  array.reverse().unshift(1);
+  return array;
+}
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(array) {
+  if(array.length < 2) return;
+  return array.join('+');
+}
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(array) {
+  return array.length + 1;
+}
