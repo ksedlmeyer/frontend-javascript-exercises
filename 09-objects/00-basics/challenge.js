1d819eedba767a95b2a285e0acd177907b92339e
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-12 03:05:57 +0000

    Objects challenge completed

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,34 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  return {
+    title: courseTitle, 
+    duration: courseDuration,
+    students: courseStudents
+  };
+}
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (object[newProp] == undefined){
+    object[newProp] = newValue;
+    return object;
+  }
+  else {
+    return object;
+  }
+}
 
-module.exports.formLetter = undefined;
 
-module.exports.canIGet = undefined;
+
+module.exports.formLetter = function(letter) {
+
+return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+}
+
+module.exports.canIGet = function(item, money) {
+  var priceTable = {};
+  priceTable['MacBook Air'] = 999;
+  priceTable['MacBook Pro'] = 1299;
+  priceTable['Mac Pro']= 2499;
+  priceTable['Apple Sticker'] = 1;
+
+  return priceTable[item] <= money;
+}
