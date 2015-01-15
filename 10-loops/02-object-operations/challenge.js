321fac57b4b6b482487596d8b89ab0dd6e189828
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-15 02:48:52 +0000

    Object operations

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,23 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object) {
+  var newObject = {};
+  for (var property in object) {
+    newObject[property] = object[property];
+  }
+  return newObject;
+}
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+  for (var property in src) {
+    dest[property] = src[property];
+  }
+  return dest;
+}
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, array) {
+  for (var index = 0; index < array.length; index++) {
+    if (typeof object[array[index]] === "undefined") {
+      return false;
+    }
+  }
+  return true;
+}
