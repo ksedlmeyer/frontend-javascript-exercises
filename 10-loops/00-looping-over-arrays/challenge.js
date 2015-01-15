a4eb727bbe08128e65b025346a4da21b261e87c7
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-15 02:45:39 +0000

    Looping over arrays

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,25 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+  if (array.length == 0) {
+    return 0
+  }
+  else {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  }
+}
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+  var comma = ','
+  return inputString.toLowerCase().split(comma);
+}
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(array) {
+  var newArray = [];
+  for (var index = 0; index < array.length; index++) {
+    newArray.push(index + " is " + array[index]);
+  }
+  return newArray;
+}
