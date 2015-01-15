f359ab995f734e245df95ec9d31613ffbd62bac5
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-15 02:47:09 +0000

    Iterating over objects

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,23 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object) {
+  var newArray = [];
+  for (var property in object) {
+    newArray.push(property);
+  }
+  return newArray;
+}
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(object) {
+  var newArray = [];
+  for (var property in object) {
+    newArray.push(object[property]);
+  }
+  return newArray;
+}
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object) {
+  var newArray = [];
+  for (var property in object) {
+    newArray.push(property + " is " + object[property]);
+  }
+  return newArray;
+}
