99fa170ae2f0201c78ff49741cafeaf8a5d2637d
Katie Sedlmeyer <katie@sedlmeyer.net>
2015-01-15 02:41:31 +0000

    While loop exercises

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,13 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+  while (conditionalFn()) {
+    actionFn();
+  }
+}
 
-module.exports.sumNumbers = undefined;
-
+module.exports.sumNumbers = function(array) {
+  var sum = 0;
+  while (array.length > 0) {
+    sum += array.shift();
+  }
+  return sum;
+}
